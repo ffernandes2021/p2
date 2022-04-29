57a436725fa30ca0fd621ba7449e493814fd9ebc
Fabio <binhobinho1988@hotmail.com>
2022-04-29 14:03:24 -0300

    full-commit

--- p2/assets/js/script.js
+++ p2/assets/js/script.js
@@ -1,27 +1,37 @@
-function getCidade(nomeCidade){  
+const localContent = $("#localContent");
+
+function isEmpty() {
+    if ($.trim($("#cidade").val()) === "") {
+        localContent.html("<h3>Informe a cidade para que a pesquisa seja realizada!</h3>");
+    } else {
+        getCidade($("#cidade").val());
+    }
+}
+
+function getCidade(nomeCidade) {
     $.ajax({
-      url: "http://servicos.cptec.inpe.br/XML/listaCidades?city=" + nomeCidade,
-      method: "get",
-      dataType: "xml"
+        url: "http://servicos.cptec.inpe.br/XML/listaCidades?city=" + nomeCidade,
+        method: "get",
+        dataType: "xml"
     })
-    .done(function(data){  
-      getPrevisaoTempoProximosQuatroDias($(data).find("id").text());
+    .done(function (data) {
+        getPrevisaoTempoProximosQuatroDias($(data).find("id").text());
     })
-    .fail(function(jqXHR){
-      console.log("Erro HTTP: " + jqXHR.status);
+    .fail(function (jqXHR) {
+        console.log("Erro HTTP: " + jqXHR.status);
     });
-  }
-        
-  function getPrevisaoTempoProximosQuatroDias(codigoCidade){
+}
+
+function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
     $.ajax({
-      url: "http://servicos.cptec.inpe.br/XML/cidade/" + codigoCidade + "/previsao.xml",
-      method: "get",
-      dataType: "xml"
+        url: "http://servicos.cptec.inpe.br/XML/cidade/" + codigoCidade + "/previsao.xml",
+        method: "get",
+        dataType: "xml"
     })
-    .done(function(data){
-      console.log(data);
+    .done(function (data) {
+        console.log(data);
     })
-    .fail(function(jqXHR){
-      console.log("Erro HTTP: " + jqXHR.status);
+    .fail(function (jqXHR) {
+        console.log("Erro HTTP: " + jqXHR.status);
     });
-  }
+}
