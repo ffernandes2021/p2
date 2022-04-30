7c6b0cf676a223ac111fa842b39c8baa4aeab3e5
Fabio <binhobinho1988@hotmail.com>
2022-04-30 17:03:00 -0300

    full-commit

--- p2/assets/js/script.js
+++ p2/assets/js/script.js
@@ -29,7 +29,11 @@ function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
         dataType: "xml"
     })
     .done(function (data) {
-        console.log(data);
+        let content = "";
+        for(let i = 0; i < 4; i++) {
+            content = content.concat("<div class=\"col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"col-md-6 col-6\">"+$(data).find("tempo").text()+"</div><div class=\"col-md-6 col-6\"><p>"+$(data).find("nome").text()+", " +$(data).find("uf").text()+"</p><p>"+$(data).find("dia").text()+"</p><p>"+$(data).find("maxima").text()+"º C | "+$(data).find("minima").text()+"º C | "+$(data).find("iuv").text()+"</p></div></div></div>");
+        }
+        localConteudo.html(content);
     })
     .fail(function (jqXHR) {
         localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
