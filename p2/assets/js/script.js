0c2402f91c21eaebb54703954fca174330506cc3
Fabio <binhobinho1988@hotmail.com>
2022-04-30 17:59:21 -0300

    full-commit

--- p2/assets/js/script.js
+++ p2/assets/js/script.js
@@ -14,12 +14,12 @@ function getCidade(nomeCidade) {
         method: "get",
         dataType: "xml"
     })
-    .done(function (data) {
-        getPrevisaoTempoProximosQuatroDias($(data).find("id").text());
-    })
-    .fail(function (jqXHR) {
-        localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
-    });
+            .done(function (data) {
+                getPrevisaoTempoProximosQuatroDias($(data).find("id").text());
+            })
+            .fail(function (jqXHR) {
+                localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
+            });
 }
 
 function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
@@ -29,11 +29,15 @@ function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
         dataType: "xml"
     })
     .done(function (data) {
+        // Cria um atributo
         let content = "";
-        for(let i = 0; i < 4; i++) {
-            content = content.concat("<div class=\"col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"col-md-6 col-6\">"+$(data).find("tempo").text()+"</div><div class=\"col-md-6 col-6\"><p>"+$(data).find("nome").text()+", " +$(data).find("uf").text()+"</p><p>"+$(data).find("dia").text()+"</p><p>"+$(data).find("maxima").text()+"º C | "+$(data).find("minima").text()+"º C | "+$(data).find("iuv").text()+"</p></div></div></div>");
+        // Faz com que o código interno às chaves ({}) seja repetido 4 vezes
+        for (let i = 0; i < 4; i++) {
+            // Adiciona à content o conteúdo do bloco anterior, totalizando assim os 4 blocos de maneira sequenciada.
+            content = content.concat("<div class=\"col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"col-md-6 col-6\">" + $(data).find("tempo").text() + "</div><div class=\"col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + $(data).find("dia").text() + "</p><p>" + $(data).find("maxima").text() + "º C | " + $(data).find("minima").text() + "º C | " + $(data).find("iuv").text() + "</p></div></div></div>");
         }
-        localConteudo.html(content);
+        // Imprime na localContent o content
+        localContent.html(content);
     })
     .fail(function (jqXHR) {
         localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
