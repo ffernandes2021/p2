3307f18ce8428560f9eadeefe2833afaaf559d8a
Fabio <binhobinho1988@hotmail.com>
2022-05-01 14:17:47 -0300

    full-commit

--- p2/assets/js/script.js
+++ p2/assets/js/script.js
@@ -31,11 +31,20 @@ function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
     .done(function (data) {
         let content = "";
         for (let i = 0; i < 4; i++) {
-            content = content.concat("<div class=\"box col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"box col-md-6 col-6\">" + $(data).find("tempo").eq(i).text() + "</div><div class=\"box col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + $(data).find("dia").eq(i).text() + "</p><p>" + $(data).find("maxima").eq(i).text() + "º C | " + $(data).find("minima").eq(i).text() + "º C | " + $(data).find("iuv").eq(i).text() + "</p></div></div></div>");
+            content = content.concat("<div class=\"box col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"box col-md-6 col-6\">" + $(data).find("tempo").eq(i).text() + "</div><div class=\"box col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + getDataFormatada($(data).find("dia").eq(i).text()) + "</p><p>" + $(data).find("maxima").eq(i).text() + "º C | " + $(data).find("minima").eq(i).text() + "º C | " + $(data).find("iuv").eq(i).text() + "</p></div></div></div>");
         }
         localContent.html(content);
     })
     .fail(function (jqXHR) {
         localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
     });
+}
+
+function getDataFormatada(dataPrevisao) {
+    const option = {
+        weekday: "short",
+        day: "numeric",
+        month: "numeric"
+    };
+    return new Date(dataPrevisao).toLocaleDateString("pt-BR", option).replace(".", "");
 }
