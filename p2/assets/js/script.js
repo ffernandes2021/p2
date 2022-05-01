47a3f322e8263a673a8a1fabdf5cb000a747a4bd
Fabio <binhobinho1988@hotmail.com>
2022-05-01 12:35:18 -0300

    full-commit

--- p2/assets/js/script.js
+++ p2/assets/js/script.js
@@ -31,7 +31,7 @@ function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
     .done(function (data) {
         let content = "";
         for (let i = 0; i < 4; i++) {
-            content = content.concat("<div class=\"box col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"box col-md-6 col-6\">" + $(data).find("tempo").text() + "</div><div class=\"box col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + $(data).find("dia").text() + "</p><p>" + $(data).find("maxima").text() + "º C | " + $(data).find("minima").text() + "º C | " + $(data).find("iuv").text() + "</p></div></div></div>");
+            content = content.concat("<div class=\"box col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"box col-md-6 col-6\">" + $(data).find("tempo").eq(i).text() + "</div><div class=\"box col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + $(data).find("dia").eq(i).text() + "</p><p>" + $(data).find("maxima").eq(i).text() + "º C | " + $(data).find("minima").eq(i).text() + "º C | " + $(data).find("iuv").eq(i).text() + "</p></div></div></div>");
         }
         localContent.html(content);
     })
