958b748e4d36c8db0477f662dc26be6f8593347a
Fabio <binhobinho1988@hotmail.com>
2022-04-30 21:19:14 -0300

    full-commit

--- p2/assets/js/script.js
+++ p2/assets/js/script.js
@@ -29,14 +29,10 @@ function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
         dataType: "xml"
     })
     .done(function (data) {
-        // Cria um atributo
         let content = "";
-        // Faz com que o código interno às chaves ({}) seja repetido 4 vezes
         for (let i = 0; i < 4; i++) {
-            // Adiciona à content o conteúdo do bloco anterior, totalizando assim os 4 blocos de maneira sequenciada.
-            content = content.concat("<div class=\"col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"col-md-6 col-6\">" + $(data).find("tempo").text() + "</div><div class=\"col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + $(data).find("dia").text() + "</p><p>" + $(data).find("maxima").text() + "º C | " + $(data).find("minima").text() + "º C | " + $(data).find("iuv").text() + "</p></div></div></div>");
+            content = content.concat("<div class=\"box col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"box col-md-6 col-6\">" + $(data).find("tempo").text() + "</div><div class=\"box col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + $(data).find("dia").text() + "</p><p>" + $(data).find("maxima").text() + "º C | " + $(data).find("minima").text() + "º C | " + $(data).find("iuv").text() + "</p></div></div></div>");
         }
-        // Imprime na localContent o content
         localContent.html(content);
     })
     .fail(function (jqXHR) {
