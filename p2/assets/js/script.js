81fbddabf4136941de88c1c1cd49ce4ef27531c8
Fabio <binhobinho1988@hotmail.com>
2022-05-01 21:27:45 -0300

    full-commit

--- p2/assets/js/script.js
+++ p2/assets/js/script.js
@@ -14,12 +14,12 @@ function getCidade(nomeCidade) {
         method: "get",
         dataType: "xml"
     })
-            .done(function (data) {
-                getPrevisaoTempoProximosQuatroDias($(data).find("id").text());
-            })
-            .fail(function (jqXHR) {
-                localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
-            });
+    .done(function (data) {
+        getPrevisaoTempoProximosQuatroDias($(data).find("id").text());
+    })
+    .fail(function (jqXHR) {
+        localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
+    });
 }
 
 function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
@@ -28,16 +28,16 @@ function getPrevisaoTempoProximosQuatroDias(codigoCidade) {
         method: "get",
         dataType: "xml"
     })
-            .done(function (data) {
-                let content = "";
-                for (let i = 0; i < 4; i++) {
-                    content = content.concat("<div class=\"box col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"box col-md-6 col-6\">" + $(data).find("tempo").eq(i).text() + "</div><div class=\"box col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + getDataFormatada($(data).find("dia").eq(i).text()) + "</p><p>" + $(data).find("maxima").eq(i).text() + "º C | " + $(data).find("minima").eq(i).text() + "º C | " + $(data).find("iuv").eq(i).text() + "</p></div></div></div>");
-                }
-                localContent.html(content);
-            })
-            .fail(function (jqXHR) {
-                localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
-            });
+    .done(function (data) {
+        let content = "";
+        for (let i = 0; i < 4; i++) {
+            content = content.concat("<div class=\"box col-md-3 col-sm-4 col-12\"><div class=\"row\"><div class=\"box col-md-6 col-6\"><iframe src=\"assets/image/" + getImagem($(data).find("tempo").eq(i).text()) + "\"></iframe></div><div class=\"box col-md-6 col-6\"><p>" + $(data).find("nome").text() + ", " + $(data).find("uf").text() + "</p><p>" + getDataFormatada($(data).find("dia").eq(i).text()) + "</p><p>" + $(data).find("maxima").eq(i).text() + "º C | " + $(data).find("minima").eq(i).text() + "º C | " + $(data).find("iuv").eq(i).text() + "</p></div></div></div>");
+        }
+        localContent.html(content);
+    })
+    .fail(function (jqXHR) {
+        localContent.html("<h3>Erro HTTP: " + jqXHR.status + "</h3>");
+    });
 }
 
 function getDataFormatada(dataPrevisao) {
